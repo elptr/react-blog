eec5dda2285ffc3bdaec5ebeefe553a3bf8c0700
administrator <lena.petrova.v@gmail.com>
2018-12-30 13:45:42 +0000

    absolute and relative pathname

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,7 +17,8 @@ class Blog extends Component {
                         <ul>
                             <li><Link to="/">Home</Link></li>
                             <li><Link to={{
-                                pathname:'/new-post',
+                                // pathname:this.props.match.url + '/new-post', //relative path
+                                pathname:'/new-post', //absolute path
                                 hash:'#submit',
                                 search:'?quick-submit=true'
                             }}>New Post</Link></li>
