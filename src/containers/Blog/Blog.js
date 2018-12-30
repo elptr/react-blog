ad4cbfd37ed505c987dcf05a6f0bb39f80c302c3
administrator <lena.petrova.v@gmail.com>
2018-12-30 13:08:44 +0000

    example of simple Routers

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 //import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -23,7 +21,8 @@ class Blog extends Component {
                     </nav>
                 </header>
 
-                <Posts />
+                <Route exact path="/" render={() => (<h1>Home</h1>)}/>
+                <Route path="/" render={() => (<h1>Home 2</h1>)}/>
             </div>
         );
     }
