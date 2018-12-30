34fec117560f885f082ea127c3bf6f2de03ea679
administrator <lena.petrova.v@gmail.com>
2018-12-30 13:55:54 +0000

    styling active navlinks

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 //import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -15,13 +15,25 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li>
+                                <NavLink
+                                exact
+                                to="/"
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color:'#fa923f',
+                                    textDecoration:'underline'
+                                }}
+                            >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li><NavLink to={{
                                 // pathname:this.props.match.url + '/new-post', //relative path
                                 pathname:'/new-post', //absolute path
                                 hash:'#submit',
                                 search:'?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
