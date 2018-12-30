d69a372a09de43881ed3c3ccefe6062c6227555b
administrator <lena.petrova.v@gmail.com>
2018-12-30 12:49:45 +0000

    install rounting

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
+//import axios from 'axios';
+import axios from '../../axios';
+
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
@@ -47,16 +50,20 @@ class Blog extends Component {
             });
         }
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+
             </div>
         );
     }
